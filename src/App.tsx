7ae77abcb428d34dd8788bb9f6fa07708cd564ae
Stefan Ruijsenaars <stefanruijsenaars@users.noreply.github.com>
2021-08-06 14:52:51 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { User  } from './model/test';
+
+const x: User = { id: 3 };
+function App() {
+  return (
+    <main>
+      Stefan + React⚛️ + Vite⚡ + Replit🌀
+    </main>
+  );
+}
+
+export default App;
